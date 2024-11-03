0551e36b9da6966a21816759cebadfdc4561aa09
brydwat <watson.bryce.d@gmail.com>
2024-11-03 13:46:42 -0700

    Simulated dataset created

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -3,3 +3,31 @@
 # Bryce Watson
 # Simulate data for analysis from Toronto Dinesafe dataset
 
+# Load libraries
+library(tidyverse)
+
+# create dataset for 60 restaurants
+n_restaurants <- 60
+
+restaurant_names <- paste("Restaurant", seq(1:60))
+
+# Generate random inspection outcomes
+set.seed(345)
+severity <- sample(c("C", "S", "M", " "),
+                   size = 60, 
+                   replace = TRUE,
+                   prob = c(0.05, 0.20, 0.55, 0.20)
+                   )
+
+# Read in csv lat/long - copied first 60 unique lat/long pairs from Dinesafe dataset
+latlong <- read.csv("data/simulated_data/lat_long.csv")
+lat <- latlong$Latitude
+long <- latlong$Longitude
+  
+# Create data frame
+dinesafe_data <- data.frame(
+  restaurant_names, 
+  severity,
+  lat,
+  long
+)
