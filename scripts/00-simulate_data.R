9e85a1a83e3364859c7e55d1ee33d97ffb4fafff
brydwat <watson.bryce.d@gmail.com>
2024-11-03 12:25:56 -0700

    Script start for data simulation and downlod

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,5 @@
+# Simulated data script
+# 11/3/2024
+# Bryce Watson
+# Simulate data for analysis from Toronto Dinesafe dataset
+
---
+++ scripts/01-download_data.R
@@ -0,0 +1,5 @@
+# Download data
+# 11/3/2024
+# Bryce Watson
+# Downloaded from: https://open.toronto.ca/dataset/dinesafe/
+
