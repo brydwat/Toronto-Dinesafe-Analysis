2e5c5d8c2ed0526d0a1ee0d0b8567e3cdfbb2fb0
brydwat <watson.bryce.d@gmail.com>
2024-11-11 19:40:40 -0700

    Script for data filtering and cleaning complete.

--- scripts/02-analyze_data.R
+++ scripts/02-analyze_data.R
@@ -1,31 +1,39 @@
-# Filtering and exploratory data analysis script
+# Filtering and cleaning
 # 11/8/2024
 # Bryce Watson
-# Filter and export dataset for mapping. Exploratory data analysis
+# Filter and export dataset for mapping. 
 
 # Load libraries
 library(tidyverse)
 
+# Initial filtering - selecting key vectors, removing rows with no inspection, updating no entry on severity to "NA".
 dinesafe_data <- read.csv("data/raw_data/Dinesafe.csv") |>
-  select(Establishment.ID, Inspection.ID, Establishment.Name, Severity, Latitude, Longitude)
+  select(Establishment.ID, Inspection.ID, Establishment.Name, Severity, Latitude, Longitude) |>
+  filter(!is.na(Inspection.ID)) |> 
+  mutate(Severity = if_else(Severity == "", "NA", Severity)) |>
+  group_by(Inspection.ID)
 
-
-
-
+# Secondary cleaning - changing severity codes to shorthand
 dinesafe_data$Severity <- recode(dinesafe_data$Severity,
-                                 "NA - Not Applicable" = "",
+                                 "NA - Not Applicable" = "NA",
                                  "M - Minor" = "M",
                                  "S - Significant" = "S",
                                  "C - Crucial" = "C")
 
-# WIP
-for (i in dinesafe_real$Establishment.ID) {
-  severity_compare <- dinesafe_real$Severity
-  cat("Severity:", severity_compare, "\n")
-  #print(severity_compare)
-  
-  while (count <= 3) {
-    cat("  Inner loop count:", count, "\n")
-    count <- count + 1
-  }
-}
+# Rank severity codes for factoring, N/A, Minor, Significant, Crucial
+severity_rank <- c("NA", "M", "S", "C")
+
+# Filter to keep only the highest severity from each establishment - 
+# another alternative is to count total codes per establishment, but 
+# will be done in another script.
+dinesafe_data <- dinesafe_data %>%
+  mutate(Severity = factor(Severity, levels = severity_rank, ordered = TRUE)) %>%
+  group_by(Inspection.ID) %>%
+  slice_max(Severity, n = 1, with_ties = FALSE) %>%
+  ungroup() %>%
+  group_by(Establishment.ID) %>%
+  slice_max(Severity, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+# Save data for later analysis
+write.csv(dinesafe_data, "data/analysis_data/dinesafe_filtered.csv")
